7216cf19a1a37d3f0d6a69c0fd97017adf36e417
Francesco Cirillo <francescocirillo2000@gmail.com>
2023-09-25 15:18:24 +0200

    Test

--- T8-G21-main/Progetto_SAD_GRUPPO21_TASK8/opt_livelli/Prototipo2.0/installazione.sh
+++ T8-G21-main/Progetto_SAD_GRUPPO21_TASK8/opt_livelli/Prototipo2.0/installazione.sh
@@ -1,14 +1,14 @@
 echo "le versioni di java e javac sono le seguenti, è necessaria la versione 1.8: "
 
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/javac
+# update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+# update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/javac
 
 java -version
 javac -version
  
-wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-1.0.6.jar
+# wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-1.0.6.jar
 
-wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-standalone-runtime-1.0.6.jar
+# wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-standalone-runtime-1.0.6.jar
 
 java -jar evosuite-1.0.6.jar
 
