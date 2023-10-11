f386997b42d5c16d1d209eb17c11abadd46417da
Francesco Cirillo <francescocirillo2000@gmail.com>
2023-10-11 20:54:40 +0200

    Updated script

--- T8-G21-main/Progetto_SAD_GRUPPO21_TASK8/robot/evosuite_test_script.sh
+++ T8-G21-main/Progetto_SAD_GRUPPO21_TASK8/robot/evosuite_test_script.sh
@@ -7,5 +7,11 @@ for FILE in /repository/*; do
     if [ "$CLASS_NAME" == "ClassUnderTest" ] || [ "$CLASS_NAME" == "Calcolatrice" ] || [ "$CLASS_NAME" == "FolderTree_v1.txt" ]; then
         continue
     fi
-    ./robot_generazione.sh $CLASS_NAME ${CLASS_NAME}SourceCode /repository/${CLASS_NAME}/${CLASS_NAME}SourceCode ${N_LEVELS}
+
+    STATISTICS_FILE="/repository/${CLASS_NAME}/RobotTest/EvoSuiteTest/01Level/TestReport/statistics.csv"
+    if [ -f "/repository/${CLASS_NAME}/RobotTest/EvoSuiteTest/01Level/TestReport/statistics.csv" ]; then
+        echo "Statistics file already exists for $CLASS_NAME, skipping..."
+    else
+        ./robot_generazione.sh "$CLASS_NAME" "${CLASS_NAME}SourceCode" "/repository/${CLASS_NAME}/${CLASS_NAME}SourceCode" "$N_LEVELS"
+    fi
 done
